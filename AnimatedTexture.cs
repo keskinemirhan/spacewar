a5eb42e1eb984aec2f7a5e1b5bf967ca48cb5aa0
keskinemirhan <keskinemirhan@gmail.com>
2024-12-12 12:42:38 +0300

    Add loop option and make frame prop public to read

--- AnimatedTexture.cs
+++ AnimatedTexture.cs
@@ -7,21 +7,23 @@ public class AnimatedTexture
     private int frameCount;
     private Texture2D myTexture;
     private float timePerFrame;
-    private int frame;
+    public int Frame { get; private set; }
     private float totalElapsed;
     private bool isPaused;
+    private bool loop;
     public float Rotation, Scale, Depth;
     public Vector2 Origin;
 
-    public AnimatedTexture(Vector2 origin, Texture2D asset, float rotation, float scale, float depth, int frameCount, int framesPerSec)
+    public AnimatedTexture(Vector2 origin, Texture2D asset, float rotation, float scale, float depth, int frameCount, int framesPerSec, bool loop = true)
     {
         this.Origin = new Vector2(origin.X / frameCount, origin.Y);
         this.Rotation = rotation;
         this.Scale = scale;
         this.Depth = depth;
         this.frameCount = frameCount;
+        this.loop = loop;
         timePerFrame = (float)1 / framesPerSec;
-        frame = 0;
+        Frame = 0;
         totalElapsed = 0;
         isPaused = false;
         myTexture = asset;
@@ -34,15 +36,16 @@ public class AnimatedTexture
         totalElapsed += elapsed;
         if (totalElapsed > timePerFrame)
         {
-            frame++;
-            frame %= frameCount;
+            Frame++;
+            if (loop) Frame %= frameCount;
+            else if (Frame >= frameCount) Frame = frameCount;
             totalElapsed -= timePerFrame;
         }
     }
 
     public void DrawFrame(SpriteBatch batch, Vector2 screenPos)
     {
-        DrawFrame(batch, frame, screenPos);
+        if (Frame < frameCount) DrawFrame(batch, Frame, screenPos);
     }
 
     public void DrawFrame(SpriteBatch batch, int frame, Vector2 screenPos)
@@ -61,7 +64,7 @@ public class AnimatedTexture
 
     public void Reset()
     {
-        frame = 0;
+        Frame = 0;
         totalElapsed = 0f;
     }
 
